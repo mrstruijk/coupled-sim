5ea442364a503cef043d0b12a64f7721adff649d
mrstruijk <mrstruijk@gmail.com>
2024-02-27 16:12:02 +0100

    Added traffic waypoints

--- Assets/_SOSXR/Scripts/WaypointDrawer.cs
+++ Assets/_SOSXR/Scripts/WaypointDrawer.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityStandardAssets.Utility;
 
@@ -6,6 +7,10 @@ using UnityStandardAssets.Utility;
 [RequireComponent(typeof(WaypointCircuit))]
 public class WaypointDrawer : MonoBehaviour
 {
+    [SerializeField] private Color m_startPointColor = Color.green;
+    [SerializeField] private Color m_pathColor = Color.blue; 
+    [SerializeField] private Color m_pointColor = Color.yellow;
+    [SerializeField] private Color m_endPointColor = Color.red;
     [SerializeField] private bool m_loopToStart;
     private WaypointCircuit _waypointCircuit;
 
@@ -24,28 +29,28 @@ public class WaypointDrawer : MonoBehaviour
             {
                 if (m_loopToStart)
                 {
-                    Gizmos.color = Color.blue;
+                    Gizmos.color = m_pathColor;
                     Gizmos.DrawLine(_waypointCircuit.Waypoints[i].transform.position, _waypointCircuit.Waypoints[0].transform.position);
                 }
 
-                Gizmos.color = Color.red;
+                Gizmos.color = m_endPointColor;
                 Gizmos.DrawCube(_waypointCircuit.Waypoints[i].transform.position, Vector3.one * 2);
             }
             else
             {
-                Gizmos.color = Color.blue;
+                Gizmos.color = m_pathColor;
                 Gizmos.DrawLine(_waypointCircuit.Waypoints[i].transform.position, _waypointCircuit.Waypoints[i + 1].transform.position);
             }
 
 
             if (i == 0)
             {
-                Gizmos.color = Color.green;
+                Gizmos.color = m_startPointColor;
                 Gizmos.DrawCube(_waypointCircuit.Waypoints[i].transform.position, Vector3.one * 2);
             }
             else
             {
-                Gizmos.color = Color.yellow;
+                Gizmos.color = m_pointColor;
                 Gizmos.DrawSphere(_waypointCircuit.Waypoints[i].transform.position, 0.25f);
             }
         }
